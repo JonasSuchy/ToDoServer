90e35620947a2fecfe4a8d20c7b8c7178cc43b75
JonasSuchy <Jonas.Suchy@gmail.com>
2022-02-16 12:16:26 +0100

    query zu requestBody geändert.

---
+++ main.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+#@app.route("/todo-list/<list_id>", methods =['get', 'post'])
+#def get_list(list_id):
+
+#if __name__ == '__main__':
+# app.run(host='0.0.0.0', port=5000)
